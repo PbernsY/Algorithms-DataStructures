52692a3e84bafde8ae14224ebfe8f37704972a4c
PbernsY <conorberns@gmail.com>
2018-08-29 18:58:51 +0100

    Linked List on steroids

--- Linkedlist.py
+++ Linkedlist.py
@@ -46,6 +46,23 @@ class LinkedList(object):
 			current_node = current_node.get_next_node()
 		return None if not current_node else current_node.data
 
+	def _sort(self):
+		if self.count() > 1:
+			new_list = []
+			current_node = self.root
+			new_list.append(current_node.data)
+			while current_node.next_node:
+				current_node = current_node.next_node
+				new_list.append(current_node.data)
+			new_list = sorted(new_list)
+			sorted_ll = LinkedList()
+			for i in range(len(new_list)):
+				sorted_ll.add(new_list[i])
+			return sorted_ll
+		else:
+			return []
+
+
 	def count(self):
 		def _count(node):
 			return 0 if not node else 1 + _count(node.next_node)
@@ -81,6 +98,32 @@ class LinkedList(object):
 		
 				return _recur_largest(ptr.next_node, largest)
 		return _recur_largest(self.root, self.root.data)
+	def iter_duplicates(self):
+		def _iter_duplicates(ptr):
+			prev = None
+			while ptr and ptr.data != prev:
+				prev = ptr.data
+				ptr = ptr.next_node
+			return ptr is not None
+		return _iter_duplicates(self.root)
+
+
+	def print_list(self):
+		current_node = self.root
+		while current_node:
+			print(current_node.data)
+			current_node = current_node.next_node
+
+	def recur_print_list(self):
+		def _recur_print(starting_node):
+			if starting_node is None:
+				return 
+			else:
+				print(starting_node.data)
+				_recur_print(starting_node.next_node)
+		return _recur_print(self.root)
+
+
 
 
 
@@ -99,12 +142,10 @@ class LinkedList(object):
 		return current_node
 
 mylist = LinkedList()
-mylist.add(5)
+mylist.add(100)
 mylist.add(5)
 mylist.add(8)
 mylist.add(12)
-print(mylist[1])
-print(mylist.recur_even_elements())
-print(mylist.recur_duplicates())
-print(mylist.recur_largest())
-print(mylist.count())
+mylist.recur_print_list()
+mylist = mylist._sort()
+mylist.print_list()
